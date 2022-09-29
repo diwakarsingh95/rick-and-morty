f1c3584a2241df3e6b53bfc5cf7e2b45198daaca
Diwakar <diwakar.singh313@gmail.com>
2022-09-29 15:36:20 +0530

    use SSR for characters page

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
-import { GET_CHARACTERS } from '../../gql/characters.gql'
+import { GET_CHARACTER, GET_CHARACTERS } from '../../gql/characters.gql'
 import Layout from '../../components/layout'
 import TopBar from '../../components/topBar'
 import { addApolloState, initializeApollo } from '../../lib/apollo-client'
@@ -7,24 +7,19 @@ import CharacterCard from '../../components/characterCard'
 import NoData from '../../components/noData'
 
 interface Props {
-  characters: {
-    info: Info
-    results: Character[]
-  }
+  character: Character
 }
 
-const Characters = ({ characters }: Props) => {
-  const { info, results } = characters
-
+const Character = ({ character }: Props) => {
   return (
     <Layout
       title='Characters - Rick & Morty'
       description='Information about all tne characters in Rick and Morty.'
     >
       <h1 className='py-4 text-center text-2xl font-bold tracking-widest md:text-3xl'>
-        Characters
+        {character.name}
       </h1>
-      {results && !!results.length ? (
+      {/* {results && !!results.length ? (
         <div className='px-5 pt-2 pb-0 md:px-10'>
           <TopBar {...info} />
           <section className='flex flex-wrap justify-center gap-5 px-5 pt-2'>
@@ -35,40 +30,34 @@ const Characters = ({ characters }: Props) => {
         </div>
       ) : (
         <NoData />
-      )}
+      )} */}
     </Layout>
   )
 }
 
-export default Characters
+export default Character
 
 export const getStaticPaths: GetStaticPaths = () => {
   return {
-    paths: [
-      {
-        params: { page: ['?page=1'] }
-      }
-    ],
+    paths: [],
     fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const queryArray = params?.page || []
-  const queryString = queryArray[queryArray.length - 1] || ''
-  const page = new URLSearchParams(queryString).get('page')
+  const id = params?.id || ''
 
   const apolloClient = initializeApollo()
   const { data } = await apolloClient.query({
-    query: GET_CHARACTERS,
+    query: GET_CHARACTER,
     variables: {
-      page: page ? parseInt(page as string) : undefined
+      id: id ? parseInt(id as string) : undefined
     }
   })
 
   return addApolloState(apolloClient, {
     props: {
-      characters: data.characters
+      character: data.character
     },
     revalidate: 3600
   })
