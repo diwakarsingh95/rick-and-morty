7b097b32ad89034393b26d2cba51f2db16f21e1b
Diwakar <diwakar.singh313@gmail.com>
2023-12-19 12:52:41 +0530

    added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import ErrorBoundary from '../components/errorBoundary'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import ErrorBoundary from "../components/errorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
-  )
+    <>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+      <SpeedInsights />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
